687251d9efea2a8bfd85bd651f9df17876558a53
Chris Schild <cvs46@nau.edu>
2018-03-17 22:52:24 -0700

    Added graceful error and cookie-based redirection

--- login/js/login.js
+++ login/js/login.js
@@ -8,7 +8,16 @@ $(".submit").click(function(event){
     type: 'POST',
     data: $formData,
     success: function(data){
-      console.log(data);
+      statusCode = data['StatusCode'];
+      console.log(statusCode);
+      if(statusCode != "0"){
+        $("#error-notify").modal('show');
+      }else {
+        token = data['Token'];
+        console.log(token);
+        document.cookie = "token=" + token;
+      }
+
     },
     error: function(data){
       console.log(data);
